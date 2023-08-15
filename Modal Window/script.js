dd0d2b2f82eff24e1878a8eea5d636601a2953ee
Otieno <ronvi035@gmail.com>
2023-08-15 22:13:21 +0300

    refactor: DRY Principle

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -6,3 +6,18 @@
  const btnShowModal = document.querySelectorAll('.show-modal');
 
 //  Working with Classes //
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i=0; i < btnShowModal.length; i++)
+btnShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
