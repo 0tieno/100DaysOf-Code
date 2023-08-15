ff789ffee40070fb0c550ea8ea8a0050b77a350a
Otieno <ronvi035@gmail.com>
2023-08-15 12:17:04 +0300

    finished on highscore logic

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -15,6 +15,7 @@ console.log(document.querySelector('.guess').value);
 // Handling Click Events //
 let secretNumber = Math.trunc(Math.random()*20 +1);
 let score = 20;
+let highScore =0;
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -33,6 +34,11 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
 
         // when guess is too high
     }else if(guess>secretNumber){
@@ -58,7 +64,7 @@ document.querySelector('.check').addEventListener('click', function(){
     }
 })
 
-
+// Again button //
 const Again = document.querySelector('.again');
 
 Again.addEventListener('click', function(){
