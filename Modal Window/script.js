5180f14de9384c3066d79683fce5dd375a5691de
Otieno <ronvi035@gmail.com>
2023-08-15 14:33:45 +0300

    declaring variables

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+ const modal = document.querySelector('.modal');
+ const overlay = document.querySelector('.overlay');
+ const btnCloseModal = document.querySelector('.close-modal');
+ const btnShowModal = document.querySelectorAll('.show-modal');
+
+//  Working with Classes //
