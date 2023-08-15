e0372001c8c079590a63c33f9d4411472136a296
Otieno <ronvi035@gmail.com>
2023-08-15 10:35:17 +0300

    started implementing the guess logic

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,6 +1,25 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+// Handling Click Events //
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = ' ⛔ No Number!'
+    }
+})
+
+
