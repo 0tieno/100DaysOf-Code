9f4dc15e8cd4759ca2b6545c59590c50b60b9402
Otieno <ronvi035@gmail.com>
2023-08-15 12:02:44 +0300

    worked on again btn

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -13,9 +13,8 @@ console.log(document.querySelector('.guess').value);
 */
 
 // Handling Click Events //
-const secretNumber = Math.trunc(Math.random()*20 +1);
+let secretNumber = Math.trunc(Math.random()*20 +1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +27,8 @@ document.querySelector('.check').addEventListener('click', function(){
         // when player wins
     }else if(guess ===  secretNumber){
         document.querySelector('.message').textContent = '🎉  Correct Number!';
+
+        document.querySelector('.number').textContent = secretNumber;
 
         document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -58,3 +59,19 @@ document.querySelector('.check').addEventListener('click', function(){
 })
 
 
+const Again = document.querySelector('.again');
+
+Again.addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20 +1);
+
+    document.querySelector('.message').textContent = 'Start Guessing...';
+
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
