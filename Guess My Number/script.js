567c5f1056a29b95004522b3b40773c085479849
Otieno <ronvi035@gmail.com>
2023-08-15 11:26:06 +0300

    implemented tthe game logic

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -13,12 +13,37 @@ console.log(document.querySelector('.guess').value);
 */
 
 // Handling Click Events //
+const secretNumber = Math.trunc(Math.random()*20 +1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if(!guess){
         document.querySelector('.message').textContent = ' ⛔ No Number!'
+    }else if(guess ===  secretNumber){
+        document.querySelector('.message').textContent = '🎉  Correct Number!'
+    }else if(guess>secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈 Too high!'
+        score--;
+        document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = '💥 You Lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    }else if (guess<secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📉 Too Low!'
+        score--;
+        document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = '💥 You Lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 })
 
