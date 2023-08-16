a9cacaa1900149cf0110cf0ed36f466dd2d8f538
Otieno <ronvi035@gmail.com>
2023-08-16 21:10:01 +0300

    chore: added hidden class to the dice

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
