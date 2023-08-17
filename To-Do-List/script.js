e638e1696a48a60ab00da79f79d98508f991e5c8
Otieno <ronvi035@gmail.com>
2023-08-17 08:50:32 +0300

    chore: script functionality

---
+++ To-Do-List/script.js
@@ -0,0 +1,18 @@
+'use strict'
+function addItem(){
+    let item = document.getElementById('box');
+    let listItem = document.getElementById('list-item');
+
+    if (item.value != ''){
+        let makeListItems = document.createElement('li');
+        makeListItems.appendChild(document.createTextNode(item.value));
+        listItem.appendChild(makeListItems);
+        item.value = '';
+
+        makeListItems.onclick = function(){
+            this.parentNode.removeChild(this);
+        }
+    }else{
+        alert('Please add an item');
+    }
+}
