099b02a6534f63602ac5d069ec96048d44ed4285
Otieno <ronvi035@gmail.com>
2023-08-17 10:31:26 +0300

    fix: small fixes on styling

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,12 +1,15 @@
 'use strict'
 
+let itemCount = 0; //Initialize the item count
+
 function addItem(){
     let item = document.getElementById('box');
     let listItem = document.getElementById('list-item'); // Corrected ID name
 
     if (item.value != ''){
         let makeListItems = document.createElement('li');
-        makeListItems.appendChild(document.createTextNode(item.value));
+        itemCount++; //increment the item count
+        makeListItems.appendChild(document.createTextNode(itemCount + '.' + item.value));
         listItem.appendChild(makeListItems);
         item.value = '';
 
