9bec5018f1fc5a9b82a7d01c80ba0feea5f67675
Otieno <ronvi035@gmail.com>
2023-09-03 19:21:52 +0300

    chore: added extra commented codes in js

--- SlideShow Corousel/script.js
+++ SlideShow Corousel/script.js
@@ -29,7 +29,8 @@ function showSlides(n) {
 
 
 
-// Function to show or hide the "Welcome to KYUSDA" text
+// ....Function to show or hide the "Welcome to KYUSDA" text....//
+
 // function showWelcomeText(show) {
 //     const welcomeText = document.querySelector('.slideshow-text');
 //     if (show) {
@@ -39,3 +40,21 @@ function showSlides(n) {
 //     }
 //   }
   
+
+
+// ..............AUTOMATIC SLIDE SHOW.............//
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
