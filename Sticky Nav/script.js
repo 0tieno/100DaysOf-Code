40cd3fe3bfd9e4539db0e4c8d53f3987619e2c52
Otieno <ronvi035@gmail.com>
2023-09-03 21:20:54 +0300

    chore: added the styles for the slideshow

--- Sticky Nav/script.js
+++ Sticky Nav/script.js
@@ -12,3 +12,71 @@ const nav = document.querySelector('#main');
     }
 
     window.addEventListener('scroll', fixNav);
+
+
+
+
+
+
+
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+// ....Function to show or hide the "Welcome to KYUSDA" text....//
+
+// function showWelcomeText(show) {
+//     const welcomeText = document.querySelector('.slideshow-text');
+//     if (show) {
+//       welcomeText.style.opacity = '1';
+//     } else {
+//       welcomeText.style.opacity = '0';
+//     }
+//   }
+  
+
+
+// ..............AUTOMATIC SLIDE SHOW.............//
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
