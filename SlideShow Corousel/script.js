71de357e862e2c797ede570ce9b874383bf11fe7
Otieno <ronvi035@gmail.com>
2023-09-06 10:13:32 +0300

    automatic slideshow js

--- SlideShow Corousel/script.js
+++ SlideShow Corousel/script.js
@@ -31,7 +31,21 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
+  if (slideIndex >= slides.length) {
+    slideIndex = 0; // Reset to the first slide
 }
+
+}
+
+ // Automatic slideshow
+function autoShowSlides() {
+  slideIndex++;
+  showSlides();
+}
+
+setInterval(autoShowSlides, 10000); // Change slide every 10 seconds (10000 milliseconds)
+
 
 
 
@@ -45,26 +59,6 @@ function showSlides(n) {
 //       welcomeText.style.opacity = '0';
 //     }
 //   }
-  
-
-
-// ..............AUTOMATIC SLIDE SHOW.............//
-
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 10000); // Change image every 10 seconds
-// }
-
 
 
 
