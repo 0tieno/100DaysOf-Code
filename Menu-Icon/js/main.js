5dfb8e7a58c3d25ac88aa9a288f1b108e24ebf86
Otieno <ronvi035@gmail.com>
2023-09-12 23:39:36 +0300

    commented the js code

--- Menu-Icon/js/main.js
+++ Menu-Icon/js/main.js
@@ -1,3 +1,20 @@
-function myFunction(x){
-    x.classList.toggle("change");
-}
+
+// const menuIcon = document.querySelector('.container');
+
+// menuIcon.addEventListener('click', () => {
+//     menuIcon.classList.toggle('change');
+// });
+
+
+
+
+// Alternate code of the same fuction commented//
+
+// Select the HTML element with the class 'container' and store it in the 'menuIcon' variable.
+const menuIcon = document.querySelector('.container');
+
+// Add an event listener to the 'menuIcon' element for the 'click' event.
+menuIcon.addEventListener('click', function() {
+    // When the 'click' event occurs, toggle the 'change' class on the 'menuIcon' element.
+    menuIcon.classList.toggle('change');
+});
